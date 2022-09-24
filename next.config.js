9d6a3592ce70d0ff126cc9c6df60223f70d420b2
emavi98 <emanuelvm04@gmail.com>
2022-09-24 09:30:25 +0200

    configure image optimization

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    /* loader: "akamai", */
-    /* path: isProd ? "https://renebitter.github.io/" : "", */
+    loader: "akamai",
+    path: "",
   },
   basePath: "/portfolio",
 };
